976e3e3dd5c112bf359dc5836f7d798216d57b4b
Simonbelete <simonbelete@gmail.com>
2023-12-28 16:49:52 +0300

    adding base path github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
   output: "export",
   distDir: "docs",
+  basePath: "/github-pages",
 };
 
 module.exports = nextConfig;
