d0a0b2769ed219625257f4ee2bf0887c6780aa0f
Simonbelete <simonbelete@gmail.com>
2023-12-29 19:02:21 +0300

    removing static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  trailingSlash: true,
-  output: "export",
-  distDir: "docs",
-  basePath: "/geez-script-fonts",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
